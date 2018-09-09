5eeb8e4fd7b199abae29a37faae990c505235899
ChaoticWeg <shawn@chaoticweg.cc>
2018-09-08 19:20:09 -0500

    Add project files.

---
+++ P2SR.NET/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P2SR.NET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
